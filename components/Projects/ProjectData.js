0ff6f2680f147f1c95f2e9524a46bf20cfe196fa
mohammed raazy <muhammadrazi51@gmail.com>
2022-05-25 17:03:33 +0700

    re-order the project display

--- components/Projects/ProjectData.js
+++ components/Projects/ProjectData.js
@@ -2,28 +2,28 @@ import { crypto, photograph, landingPage, realEstate } from "../../assets/img"
 
 export const projectInfo = [
     { 
-        id: 1, img: photograph, 
-        title: 'Photograph App', 
-        desc: 'i build this lovely photograph app using nextjs, reactjs and tailwindcss',
-        // stacks:[
-        //     { id: 1, names: 'Reactjs' },
-        //     { id: 2, names: 'Tailwindcss' },
-        // ],
-        liveName: 'Live Site', 
-        liveLink: 'https://photograph-app.vercel.app/', sourceName: 'Source Code', 
-        sourceLink: 'https://github.com/muhammadraazy/Photograph-App'
-    },
-    { 
-        id: 2, img: crypto, 
+        id: 1, img: crypto, 
         title: 'CryptoApp', 
         desc: 'building a cryptocurrency info app with reactjs, redux toolkit, ant design, and axios',
         // stacks:[
-        //     { id: 1, names: 'Reactjs' },
-        //     { id: 2, names: 'Tailwindcss' },
-        // ],
-        liveName: 'Live Site', 
-        liveLink: 'https://cryptos-info.netlify.app/', sourceName: 'Source Code', sourceLink: 'https://github.com/muhammadraazy/crypto-app'
+            //     { id: 1, names: 'Reactjs' },
+            //     { id: 2, names: 'Tailwindcss' },
+            // ],
+            liveName: 'Live Site', 
+            liveLink: 'https://cryptos-info.netlify.app/', sourceName: 'Source Code', sourceLink: 'https://github.com/muhammadraazy/crypto-app'
     },
+    { 
+            id: 2, img: photograph, 
+            title: 'Photograph App', 
+            desc: 'i build this lovely photograph app using nextjs, reactjs and tailwindcss',
+            // stacks:[
+            //     { id: 1, names: 'Reactjs' },
+            //     { id: 2, names: 'Tailwindcss' },
+            // ],
+            liveName: 'Live Site', 
+            liveLink: 'https://photograph-app.vercel.app/', sourceName: 'Source Code', 
+            sourceLink: 'https://github.com/muhammadraazy/Photograph-App'
+        },
     { 
         id: 3, img: landingPage, 
         title: 'Landing Page',
